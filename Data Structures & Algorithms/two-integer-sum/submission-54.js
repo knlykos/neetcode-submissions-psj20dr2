3934d5c510529536394179107f40b0e8fdb3fcc1
Nefi López García <nlopezg87@gmail.com>
2026-04-10 05:11:36 -0700

    Add: two-integer-sum - submission-54

---
+++ Data Structures & Algorithms/two-integer-sum/submission-54.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const vistos = new Map();
+
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if (vistos.has(diff)) {
+                return [i, vistos.get(diff)]
+            }
+            vistos.set(nums[i], i);
+            console.log(vistos)
+        }
+    }
+}
