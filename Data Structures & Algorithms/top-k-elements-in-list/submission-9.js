2ada4df05570c5ea7442399b5c4cccf883d042d0
Nefi López García <nlopezg87@gmail.com>
2026-04-22 05:58:02 -0700

    Add: top-k-elements-in-list - submission-9

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-9.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        if (nums.length <= 0) return []
+        const map = new Map();
+        let freqBuckets = [];
+        for (let i = 0; i < nums.length; i++) {
+            if (!map.has(nums[i])) {
+                map.set(nums[i], 0);
+            }
+            map.set(nums[i], (map.get(nums[i]) || 0) + 1)
+        }
+        map.forEach((v, k) => {
+            if (!freqBuckets[v - 1]) {
+                freqBuckets[v - 1] = [];
+            }
+            freqBuckets[v - 1].push(k)
+        })
+        freqBuckets = freqBuckets.flat()
+        return freqBuckets.slice(-k)
+    }
+}
